17ec10c763aba4e6397a2f5edc14b67f6b838968
Sete <elsete79@gmail.com>
2018-06-07 18:29:33 +0200

    Animaciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,44 @@ import { Component, OnInit } from '@angular/core';
 import { DeckService, IDeckType } from '../app/services/DeckService';
 import { Card } from './model/Card';
 import { Player } from './model/Player';
-
+import { trigger, state, style, animate, transition, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  animations: [
+    trigger('flyInOut', [
+      state('enter', style({transform: 'translateX(0)'})),
+      transition('void => in', [
+        animate(3000, keyframes([
+          style({opacity: 0, transform: 'translateX(-100%)', offset: 0}),
+          style({opacity: 1, transform: 'translateX(15px)',  offset: 0.3}),
+          style({opacity: 1, transform: 'translateX(0)',     offset: 1.0}),
+        ]))
+      ]),
+      transition('void => in2', [
+        animate(1000, keyframes([
+          style({opacity: 0, transform: 'translateX(-100%)', offset: 0}),
+          style({opacity: 1, transform: 'translateX(15px)',  offset: 0.3}),
+          style({opacity: 1, transform: 'translateX(0)',     offset: 1.0}),
+        ]))
+      ]),
+      transition('void => *', [
+        animate(300, keyframes([
+          style({opacity: 0, transform: 'translateX(-100%)', offset: 0}),
+          style({opacity: 1, transform: 'translateX(15px)',  offset: 0.3}),
+          style({opacity: 1, transform: 'translateX(0)',     offset: 1.0}),
+        ]))
+      ]),
+      transition('* => void', [
+        animate(300, keyframes([
+          style({opacity: 1, transform: 'translateX(0)',     offset: 0}),
+          style({opacity: 1, transform: 'translateX(-15px)', offset: 0.7}),
+          style({opacity: 0, transform: 'translateX(100%)',  offset: 1.0}),
+        ]))
+      ])
+    ])
+  ]
 })
 export class AppComponent implements OnInit {
 
@@ -53,6 +86,10 @@ export class AppComponent implements OnInit {
     }
 
     return nextCards;
+  }
+
+  public animationStarted(e) {
+    console.log(e);
   }
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
 import { DeckService } from 'src/app/services/DeckService';
 import { CardComponent } from './components/card/card/card.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
@@ -11,7 +15,8 @@ import { CardComponent } from './components/card/card/card.component';
     CardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
   ],
   providers: [DeckService],
   bootstrap: [AppComponent]
